6e92d212c51f847cfd82b2d495c2599ffaab9fe2
naziradenushka <59218086+naziradenushka@users.noreply.github.com>
2021-12-24 21:55:59 +0500

    3-module 5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ');
+  let min = 0;
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(arr[i]) && parseFloat(arr[i]) < min) {
+      min = parseFloat(arr[i]);
+    }
+    if (!isNaN(arr[i]) && parseFloat(arr[i]) > max) {
+      max = parseFloat(arr[i]);
+    }
+  }
+  return {
+    min, max
+  };
 }
