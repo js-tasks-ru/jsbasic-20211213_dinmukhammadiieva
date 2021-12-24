91b7746fce30aba3e346eb9351416ce9eda4895f
naziradenushka <59218086+naziradenushka@users.noreply.github.com>
2021-12-24 21:33:28 +0500

    3-module 2-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,9 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let array = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= a && arr[i] <= b) {
+      array.push(arr[i]);
+    }
+  }
+  return array;
 }
