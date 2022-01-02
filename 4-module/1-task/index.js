2acb41269445ed4e2b9eb924cafd16803d7d6318
naziradenushka <59218086+naziradenushka@users.noreply.github.com>
2022-01-02 10:30:02 +0500

    4-module 1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,7 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  for (let i = 0; i < friends.length; i++) {
+    ul.innerHTML = ul.innerHTML + `<li>${ friends[i].firstName } ${ friends[i].lastName }</li>`;
+  }
+  return ul;
 }
