7e1ffe47dca490f864729236e66038257dd93a2a
naziradenushka <59218086+naziradenushka@users.noreply.github.com>
2022-01-03 11:29:26 +0500

    4-module 1-2-task change with i=one cycle

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,9 +1,5 @@
 function makeDiagonalRed(table) {
   for (let i = 0; i < table.rows.length; i++) {
-    for (let j = 0; j < table.rows[i].cells.length; j++) {
-      if (i == j) {
-        table.rows[i].cells[j].style.backgroundColor = "red";
-      }
-    }
+    table.rows[i].cells[i].style.backgroundColor = "red";
   }
 }
